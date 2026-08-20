f5e1337d0a6104d147f8d664662abe7df96e6e0c
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-08-20 22:33:52 +0700

    Complete first C++ program

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 
 int main()
 {
+    std::cout << "==============================" << std::endl;
+    std::cout << "       MY FIRST C++ APP       " << std::endl;
+    std::cout << "==============================" << std::endl;
+
     std::cout << "Hello, C++!" << std::endl;
+    std::cout << "Course: Object-Oriented Programming" << std::endl;
+    std::cout << std::endl;
+    std::cout << "I'm ready to learn C++!" << std::endl;
 
     return 0;
 }
